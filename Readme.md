399ed639d92068bf5a6809b4a0057d74e957faee
Danny_lemoo <73852343+DannyLemoo@users.noreply.github.com>
2022-06-03 12:42:27 -0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# LADS
+## _Create your own ads_
+
+
+New version out! (v0.2)
+
+- New ads support
+- Images support
+- Anti-adblocker and safe
+
+## Instrucions
+
+- Create a index.html
+- Then, write the following code: <script src="./kit.js"></script>
+- Create a index.js
+- On it, type: ads.create()
+- And then, enjoy!
+
+## Technology
+
+LADS uses:
+
+- [Java Script] - For creating the ads (Written on vanilla js)
+- [HTML] - for the base
+- And many more!
+
+
+## Installation
+
+You don't need to install anything!
+
+## Copyright® LADS
---
+++ kit.js
@@ -0,0 +1,120 @@
+var templates = ['ad.png', '111.png'];
+var con = document.getElementById('anuncios');
+
+//Crea una variable llamada src que obtenga una imagen de templates aleatoria
+var src = templates[Math.floor(Math.random() * templates.length)];
+var adblockText = 'We appreciate you!<br>';
+var adblockSub = 'But we need your help to make this <br> site better!';
+
+
+var ads = {
+    'create': function(position){
+        var anuncio = document.createElement('img');
+        anuncio.src = src;
+
+        //add the id ad to the element
+        anuncio.id = 'ad';
+        //add the class ad to the element
+        anuncio.className = 'ad';
+        anuncio.style.maxWidth = '400px';
+        anuncio.style.maxHeight = '250px';
+        src = templates[Math.floor(Math.random() * templates.length)];
+        if(position == 'top'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.top = '0';
+            anuncio.style.left = '0';
+            anuncio.style.right = '0';
+            anuncio.style.zIndex = '9999';
+        }
+        else if(position == 'bottom'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.bottom = '0';
+            anuncio.style.left = '0';
+            anuncio.style.right = '0';
+            anuncio.style.zIndex = '9999';
+        }
+        else if(position == 'left'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.top = '50%';
+            anuncio.style.transform = 'translateY(-50%)';
+            anuncio.style.left = '0';
+            anuncio.style.bottom = '0';
+            anuncio.style.zIndex = '9999';
+        }
+        else if(position == 'right'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.top = '50%';
+            anuncio.style.transform = 'translateY(-50%)';
+            anuncio.style.right = '0';
+            anuncio.style.bottom = '0';
+            anuncio.style.zIndex = '9999';
+        } else if(position == 'top-left'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.top = '0';
+            anuncio.style.left = '0';
+            anuncio.style.zIndex = '9999';
+        }
+        else if(position == 'top-right'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.top = '0';
+            anuncio.style.right = '0';
+            anuncio.style.zIndex = '9999';
+        }
+        else if(position == 'bottom-left'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.bottom = '0';
+            anuncio.style.left = '0';
+            anuncio.style.zIndex = '9999';
+        }
+        else if(position == 'bottom-right'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.bottom = '0';
+            anuncio.style.right = '0';
+            anuncio.style.zIndex = '9999';
+        }
+        else if(position == 'top-center'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.top = '0';
+            anuncio.style.left = '50%';
+            anuncio.style.transform = 'translateX(-50%)';
+            anuncio.style.zIndex = '9999';
+        }
+        else if(position == 'bottom-center'){
+            anuncio.style.position = 'fixed';
+            anuncio.style.bottom = '0';
+            anuncio.style.left = '50%';
+            anuncio.style.transform = 'translateX(-50%)';
+            anuncio.style.zIndex = '9999';
+        }
+        setTimeout(function(){
+            document.body.appendChild(anuncio);
+        }, 1)
+    },
+    'load': function(){
+        console.log('Powered by TADS Kit');
+    },
+    'get': function(){
+        //Console.log the number of id's ad
+        console.log(document.getElementsByClassName('ad').length);
+    },
+    'static': function(x, y){
+        var anuncio = document.createElement('img');
+        anuncio.src = src;
+
+        //add the id ad to the element
+        anuncio.id = 'ad';
+        //add the class ad to the element
+        anuncio.className = 'ad';
+        anuncio.style.maxWidth = '400px';
+        anuncio.style.maxHeight = '250px';
+
+        anuncio.style.position = 'absolute';
+        anuncio.style.top = y + 'px';
+        anuncio.style.left = x + 'px';
+        anuncio.style.zIndex = '9999';
+        setTimeout(function(){
+            document.body.appendChild(anuncio);
+        },1)
+    },
+}
+window.onload = ads.load();
